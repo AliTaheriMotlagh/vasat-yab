6ca3905d9843298d5509a235860c6d5fe956429b
Ali Taheri Motlagh <alitaherimotlagh@yahoo.com>
2024-02-12 02:36:28 +0330

    adding auth from tutorial video until register functionality

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,8 +8,6 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+export const db = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
