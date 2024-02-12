3a417ec98ad8d27a17c6c6a4191e3e3bb2e8751e
Ali Taheri Motlagh <alitaherimotlagh@yahoo.com>
2024-02-12 19:12:55 +0330

    mail

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -2,12 +2,11 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
-  console.log("2FA Code ", token);
-  
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `mail@${domain}`,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -15,12 +14,10 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
-
-  console.log("Reset your password ", resetLink);
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `mail@${domain}`,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -28,12 +25,10 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
-
-  console.log("Confirm your email ", confirmLink);
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `mail@${domain}`,
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
