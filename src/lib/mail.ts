8173d7207334988f37ab3021e282e25e7bd9f477
Ali Taheri Motlagh <alitaherimotlagh@yahoo.com>
2024-02-15 19:04:22 +0330

    await in mail

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -20,13 +20,13 @@ const sendMail = async (email: string, subject: string, html: string) => {
 };
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
-  sendMail(email, "2FA Code", `<p>Your 2FA code: ${token}</p>`);
+  await sendMail(email, "2FA Code", `<p>Your 2FA code: ${token}</p>`);
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  sendMail(
+  await sendMail(
     email,
     "Reset your password",
     `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
@@ -36,7 +36,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  sendMail(
+  await sendMail(
     email,
     "Confirm your email",
     `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
