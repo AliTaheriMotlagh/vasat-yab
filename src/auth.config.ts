b992a3ad8790143d8ef1508c3239db03cf6ca633
Ali Taheri Motlagh <alitaherimotlagh@yahoo.com>
2024-02-18 22:09:24 +0330

    remove github provider auth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,6 @@
 import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 import { LoginSchema } from "@/schemas";
@@ -12,10 +11,6 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    Github({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     Credentials({
       async authorize(credentials) {
--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -2,7 +2,6 @@
 
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
 import { useSearchParams } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
@@ -12,7 +11,7 @@ export const Social = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
 
-  const onClick = (provider: "google" | "github") => {
+  const onClick = (provider: "google") => {
     signIn(provider, {
       callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     });
@@ -26,13 +25,6 @@ export const Social = () => {
         variant="outline"
         onClick={() => onClick("google")}>
         <FcGoogle className="h-5 w-5" />
-      </Button>
-      <Button
-        size="lg"
-        className="w-full"
-        variant="outline"
-        onClick={() => onClick("github")}>
-        <FaGithub className="h-5 w-5" />
       </Button>
     </div>
   );
