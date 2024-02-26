10baef39fd79460c725e7d36dab8274662a5718f
Ali Taheri Motlagh <alitaherimotlagh@yahoo.com>
2024-02-26 18:22:28 +0330

    rooms menu item

--- src/components/auth/user-button.tsx
+++ src/components/auth/user-button.tsx
@@ -14,7 +14,7 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { logout } from "@/actions/logout";
 import Link from "next/link";
-import { MailIcon } from "lucide-react";
+import { MailIcon, MapPinned } from "lucide-react";
 
 export const UserButton = () => {
   const user = useCurrentUser();
@@ -27,7 +27,7 @@ export const UserButton = () => {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src={user?.image || ""} />
+          <AvatarImage src={user?.image!} />
           <AvatarFallback className="bg-sky-500">
             <FaUser className="text-white" />
           </AvatarFallback>
@@ -35,6 +35,13 @@ export const UserButton = () => {
       </DropdownMenuTrigger>
 
       <DropdownMenuContent className="w-auto" align="end">
+        <Link href="/rooms" className="cursor-pointer">
+          <DropdownMenuItem>
+            <MapPinned className="mr-2 h-4 w-4" />
+            <span>Rooms</span>
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator />
         <Link href="/notifications" className="cursor-pointer">
           <DropdownMenuItem>
             <MailIcon className="mr-2 h-4 w-4" />
